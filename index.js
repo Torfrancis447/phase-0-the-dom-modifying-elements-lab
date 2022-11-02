40de7e87d1b44aacd7bca87a7bc2aaa4955b69b9
tor <torfrancis447@gmail.com>
2022-11-02 16:06:29 -0400

    commit tree 0e58f76bf87e13dccb330df8230318716495985f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainTag= document.querySelector('#main');
+mainTag.remove();
+const newHeader= document.createElement('h1')
+newHeader.id= 'victory'
+newHeader.textContent = 'Tor is the champion'
+function changeName(name) {
+
+}
